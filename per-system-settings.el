9d9ac8fe1559af0f5d7d1780199deff4989b97b1
Daniel Newman <dwnewman78@gmail.com>
2021-06-16 09:34:57 -0500

    Added update scripts and per system settings

--- per-system-settings.el
+++ per-system-settings.el
@@ -1,8 +1,14 @@
 (require 'map) ;; Needed for map-merge
 
-(setq dw/system-settings
+(setq dn/system-settings
   (map-merge
     'list
     '((emacs/default-face-size . 220)
       (emacs/variable-face-size . 245)
-      (emacs/fixed-face-size . 200))))
+      (emacs/fixed-face-size . 200))
+    
+    (when (equal system-name "sunstreaker")
+      '((emacs/default-face-size . 190)
+        (emacs/variable-face-size . 200)
+        (emacs/fixed-face-size . 190)))
+    ))
--- tangle-emacs-config.el
+++ tangle-emacs-config.el
@@ -1,19 +1,19 @@
-  (require 'org)
-  (load-file "lisp/dw-settings.el")
+(require 'org)
+(load-file "lisp/dw-settings.el")
 
-  ;; Don't ask when evaluating code blocks
-  (setq org-confirm-babel-evaluate nil)
+;; Don't ask when evaluating code blocks
+(setq org-confirm-babel-evaluate nil)
 
-  (let* ((dotfiles-path (expand-file-name "."))
-	 (org-files (directory-files dotfiles-path nil "\\.org$")))
+(let* ((dotfiles-path (expand-file-name "."))
+       (org-files (directory-files dotfiles-path nil "\\.org$")))
 
-    (defun dw/tangle-org-file (org-file)
-      (message "\n\033[1;32mUpdating %s\033[0m\n" org-file)
-      (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))
+  (defun dw/tangle-org-file (org-file)
+    (message "\n\033[1;32mUpdating %s\033[0m\n" org-file)
+    (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))
 
-    ;; Tangle Systems.org first
-    (dw/tangle-org-file "Systems.org")
+  ;; Tangle Systems.org first
+  (dw/tangle-org-file "Systems.org")
 
-    (dolist (org-file org-files)
-      (unless (member org-file '("README.org" "Systems.org"))
-      	(dw/tangle-org-file org-file))))
+  (dolist (org-file org-files)
+    (unless (member org-file '("README.org" "Systems.org"))
+      (dw/tangle-org-file org-file))))
