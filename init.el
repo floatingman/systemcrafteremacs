770acdc0ce2611c5b10113c52c11927f5f190dec
Daniel Newman <daniel.newman@teamexos.com>
2021-11-16 17:28:30 -0600

    Changed Emacs color theme

--- init.el
+++ init.el
@@ -176,10 +176,9 @@
 
 (setq ad-redefinition-action 'accept)
 
-(use-package spacegray-theme :defer t)
-(use-package doom-themes :defer t)
-(load-theme 'doom-gruvbox t)
-(doom-themes-visual-bell-config)
+(use-package base16-theme
+  :config
+  (load-theme 'base16-tomorrow t))
 
 ;; Set the font face based on platform
 (pcase system-type
@@ -199,7 +198,7 @@
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil
                     ;; :font "Cantarell"
-                    :font "Iosevka"
+                    :font "Iosevka Aile"
                     :height (dw/system-settings-get 'emacs/variable-face-size)
                     :weight 'light)
 
@@ -455,17 +454,17 @@
 	  (plist-put ivy-rich-display-transformers-list
 		     'ivy-switch-buffer
 		     '(:columns
-		 ((ivy-rich-candidate (:width 40))
+		       ((ivy-rich-candidate (:width 40))
 			(ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
 			(ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
 			(ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
 			(ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
-		 :predicate
-		 (lambda (cand)
+		       :predicate
+		       (lambda (cand)
 			 (if-let ((buffer (get-buffer cand)))
 			     ;; Don't mess with EXWM buffers
 			     (with-current-buffer buffer
-			 (not (derived-mode-p 'exwm-mode)))))))))
+			       (not (derived-mode-p 'exwm-mode)))))))))
 (use-package counsel
   :demand t
   :bind (("M-x" . counsel-M-x)
