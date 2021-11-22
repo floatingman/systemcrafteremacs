065966d264fa572b9d2f194930b4fffb59347024
Daniel Newman <daniel.newman@teamexos.com>
2021-11-22 12:23:09 -0600

    Removed unused settings

--- tangle-emacs-config.el
+++ tangle-emacs-config.el
@@ -1,5 +1,4 @@
 (require 'org)
-(load-file "~/.emacs.d/lisp/dw-settings.el")
 
 ;; Don't ask when evaluating code blocks
 (setq org-confirm-babel-evaluate nil)
@@ -7,13 +6,13 @@
 (let* ((dotfiles-path (expand-file-name "."))
        (org-files (directory-files dotfiles-path nil "\\.org$")))
 
-  (defun dw/tangle-org-file (org-file)
+  (defun dn/tangle-org-file (org-file)
     (message "\n\033[1;32mUpdating %s\033[0m\n" org-file)
     (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))
 
   ;; Tangle Systems.org first
-  (dw/tangle-org-file "Systems.org")
+  (dn/tangle-org-file "Systems.org")
 
   (dolist (org-file org-files)
     (unless (member org-file '("README.org" "Systems.org"))
-      (dw/tangle-org-file org-file))))
+      (dn/tangle-org-file org-file))))
